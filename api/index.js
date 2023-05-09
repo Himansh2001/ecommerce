ecff37034bbbabe5d09728db46a321e651a0fae5
unknown <tiwarihimanshu025@gmail.com>
2023-05-09 16:57:02 +0530

    frontend and backend done

---
+++ api/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const userRoute = require("./routes/user")
+const authRoute=require("./routes/auth")
+const productRoute=require("./routes/product")
+const CartRoute=require("./routes/cart")
+const OrderRoute=require("./routes/order")
+app.use(express.json()) ;
+
+
+app.use("/api/auth",authRoute) ;
+app.use("/api/users",userRoute) ;
+app.use("/api/products",productRoute) ;
+app.use("/api/carts",CartRoute) ;
+app.use("/api/orders",OrderRoute);
+
+
+const connect =async()=>{
+   try{
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("connected to db");
+    app.listen(process.env.PORT||5000,()=>{
+        console.log("listeninig on port 5000...") ;
+    });
+    
+   }catch(err){
+    console.log(err) ;
+   }
+
+}
+
+connect() ;
